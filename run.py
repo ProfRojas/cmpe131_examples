1d41e485ab67aaeda13d4aa01cb65f91739060ee
Carlos Rojas <carlos.rojas@sjsu.edu>
2021-03-08 08:31:35 -0800

    Output html in route hello with variable in string

--- run.py
+++ run.py
@@ -9,14 +9,23 @@ myapp_obj = Flask(__name__)
 @myapp_obj.route("/")
 # called view function
 def hello():
+
+    user = {'name' : 'Miguel (made with Dictionary)'}
     # return HTML string
-    return '''
+    #
+    # Remember when using quotes inside of quotes you need to change.
+    # For example, as shown below if the outside quotes are ' then any quotes
+    # inside that string need to be " quotes.
+    #
+    # Also, I can print variables inside the string using {} because I put an
+    # f in front of the start of the string.
+    return f'''
     <html>
     <head>
         <title>Home Page - my blog</title>
     </head>
     <body>
-        <h1>Hello, Miguel !</h1>
+        <h1>Hello, {user["name"]} !</h1>
     </body>
     </html>'''
 
