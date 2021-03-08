7073582a49c0a1a55e77146c492697014ae19ba5
Carlos Rojas <carlos.rojas@sjsu.edu>
2021-03-08 12:14:35 -0800

    Use template inheritance on hello.html

--- app/routes.py
+++ app/routes.py
@@ -8,4 +8,15 @@ from app import myapp_obj
 # called view function
 def hello():
     user = {'name' : 'Miguel (made with Dictionary)'}
-    return render_template('hello.html', user_template=user)
+    posts = [
+        {
+            'author' : 'Linh',
+            'body' : 'Beautiful day in San Jose!'
+        },
+        {
+            'author': 'Emma',
+            'body' : 'I got my vaccine today!'
+        }
+    ]
+
+    return render_template('hello.html', user_template=user, posts=posts)
