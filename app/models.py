238703748ce7f9ab87e1427a3f4d490e9c849f19
Carlos Rojas <carlos.rojas@sjsu.edu>
2021-03-15 10:18:17 -0700

    Add relationship between User and Post models

--- app/models.py
+++ app/models.py
@@ -9,6 +9,8 @@ class User(db.Model):
     email = db.Column(db.String(32), unique=True, nullable=False, index=True)
     password = db.Column(db.String(200), unique=False)
 
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
     def __repr__(self):
         return f'<{self.username}>'
 
@@ -17,6 +19,7 @@ class Post(db.Model):
     body = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Posts {}>'.format(self.body)
