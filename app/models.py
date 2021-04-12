d49042dda241e994b31b661fefab18dc96afd2c9
Carlos Rojas <carlos.rojas@sjsu.edu>
2021-04-12 14:05:24 -0700

    Add login check

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 # same as
 # from app import db
-from . import db
+from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,9 +12,15 @@ class User(db.Model):
 
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
-    def __repr__(self):
-        return f'<{self.username}>'
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)    
+        
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(256))
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, flash, redirect
 from app import myapp_obj
 from app.forms import LoginForm
 
+from app.models import User
 
 # different URL the app will implement
 @myapp_obj.route("/")
@@ -26,6 +27,16 @@ def hello():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        # User.query.filter_by() returns a list from the User table
+        # first() returns first element of the list
+        # the form.username.data is getting the info the user submitted in the form
+        user = User.query.filter_by(username=form.username.data).first()
+        # if no user found or password for user incorrect
+        # user.check_password() is a method in the User class
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect('/login')
+        
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect('/')
